69fea5d378ba02672b350230f9c5529a942f7165
lap2ka <83134094+lap2ka@users.noreply.github.com>
2026-06-23 16:27:43 +0200

    Nice to have

--- src/main/java/me/cortex/voxy/client/mixin/minecraft/MixinMinecraft.java
+++ src/main/java/me/cortex/voxy/client/mixin/minecraft/MixinMinecraft.java
@@ -2,6 +2,7 @@ package me.cortex.voxy.client.mixin.minecraft;
 
 import me.cortex.voxy.client.ClientSessionEvents;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.screens.Screen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -9,8 +10,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Minecraft.class)
 public class MixinMinecraft {
-    @Inject(method = "disconnect", at = @At("TAIL"))
-    private void voxy$injectWorldClose(CallbackInfo ci) {
+    @Inject(method = "disconnect(Lnet/minecraft/client/gui/screens/Screen;Z)V", at = @At("TAIL"))
+    private void voxy$injectWorldClose(Screen screen, boolean transferring, CallbackInfo ci) {
         if (ClientSessionEvents.inSession) {
             ClientSessionEvents.sessionEnd();
         }
